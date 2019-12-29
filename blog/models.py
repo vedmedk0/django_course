6a1b919dd1777367fefe49755f94e2ab43adab1b
Vedmedk0 <Vedmedk0@mail.ru>
2019-12-29 16:01:29 +0300

    blog category

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    """Модель категории"""
+    name = models.CharField(verbose_name='Имя', max_length=100)
+    slug = models.SlugField(verbose_name='url', max_length=100)  # то что хранится по url
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = "Категории"
