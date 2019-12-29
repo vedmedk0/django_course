19edf876138f4f516c93341f4734dc62f7fc5b45
Vedmedk0 <Vedmedk0@mail.ru>
2019-12-29 17:59:43 +0300

    render categories

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import View
+from .models import Category
+
 
 # Create your views here.
+
+# def home(request):
+#     if request.method == "POST":
+#         return HttpResponse('Hi')
+#     elif request.method == "GET":
+#         return HttpResponse("good")
+
+
+class Homeview(View):
+    def get(self, request):
+        category_list = Category.objects.all()
+        return render(request, 'blog/home.html', {'categories': category_list})
+
+
+class CategoryView(View):
+    """Вывод статей категории"""
+
+    def get(self, request, category_name):
+        category = Category.objects.get(slug=category_name)
+        return render(request, 'blog/post_list.html', {'category': category})
